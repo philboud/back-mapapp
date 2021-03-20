4e154b040b6667fef6ba6a79fdb988af21f797d3
Philippe.boudet <philippe.boudet42@gmail.com>
2021-03-20 16:44:42 +0100

    Chat interface date type

--- models/chat.model.js
+++ models/chat.model.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema
 var ChatSchema = new Schema({
   user: String,
   message: String,
-  timeStamp: String
+  timeStamp: { type: Date, default: Date.now }
 });
 
 var Chat = mongoose.model("Chat", ChatSchema)
